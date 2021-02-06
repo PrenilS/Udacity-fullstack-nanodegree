d3ad261e7ee929fef1b4dc7fb05d077eefb6dfab
PrenilS <prenil.s@gmail.com>
2021-02-07 00:44:05 +0200

    feat: Add create functionality

--- TODO app/todo.py
+++ TODO app/todo.py
@@ -1,11 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for, redirect
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://postgres:postgres@localhost:5432/todoapp'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 db = SQLAlchemy(app)
-db.init_app(app)
+
 class TODO(db.Model):
     __tablename__ = 'todos'
     id = db.Column(db.Integer, primary_key=True)
@@ -16,6 +16,17 @@ class TODO(db.Model):
 
 db.create_all()
 
+@app.route('/todos/create', methods = ['POST'])
+def create():
+    description = request.form.get('description')
+    todo = TODO(description = description)
+    db.session.add(todo)
+    db.session.commit()
+    return redirect(url_for('index'))
+
+
+
 @app.route('/')
 def index():
     return render_template('index.html', data=TODO.query.all())
+
