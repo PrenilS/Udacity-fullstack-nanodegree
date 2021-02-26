e4db34dc54a03e68e3344929d7ed283d070121ca
Prenil <prenils@lightstone.co.za>
2021-02-26 16:50:47 +0200

    feat: add error handling for submit

--- TODO app/todo.py
+++ TODO app/todo.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, url_for, redirect, jsonify
+from flask import Flask, render_template, request, url_for, redirect, jsonify, abort
 from flask_sqlalchemy import SQLAlchemy
 import sys
 
@@ -19,13 +19,26 @@ db.create_all()
 
 @app.route('/todos/create', methods = ['POST'])
 def create():
-    description = request.get_json()['description']
-    todo = TODO(description=description)
-    db.session.add(todo)
-    db.session.commit()
-    return jsonify({
-        'description': todo.description
-    })
+    error = False
+    body = {}
+    try:
+        description = request.get_json()['description']
+        todo = TODO(description=description)
+        db.session.add(todo)
+        db.session.commit()
+        body = {
+            'description': todo.description
+        }
+    except:
+        error = True
+        db.session.rollback()
+        print(sys.exc_info())
+    finally:
+        db.session.close()
+    if error:
+        abort(400)
+    else:
+        return jsonify(body)
 
 
 
