c544cd1d03d034a9e7eb5df184f55d3128564a08
Prenil <prenils@lightstone.co.za>
2021-02-27 14:47:02 +0200

    feat: add ability to check items

--- TODO app/todo.py
+++ TODO app/todo.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, url_for, redirect, jsonify, abort
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 import sys
 
 app = Flask(__name__)
@@ -7,15 +8,18 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://postgres:postgres@localhost:
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 db = SQLAlchemy(app)
 
+migrate = Migrate(app, db)
+
 class TODO(db.Model):
     __tablename__ = 'todos'
     id = db.Column(db.Integer, primary_key=True)
     description = db.Column(db.String(), nullable=False)
+    completed = db.Column(db.Boolean, nullable=True)
 
     def __repr__(selfself):
         return f'<Todo {self.id} {self.description}>'
 
-db.create_all()
+#db.create_all()
 
 @app.route('/todos/create', methods = ['POST'])
 def create():
@@ -23,7 +27,7 @@ def create():
     body = {}
     try:
         description = request.get_json()['description']
-        todo = TODO(description=description)
+        todo = TODO(description=description, completed=False)
         db.session.add(todo)
         db.session.commit()
         body = {
@@ -40,9 +44,22 @@ def create():
     else:
         return jsonify(body)
 
+@app.route('/todos/<todo_id>set-completed', methods = ['POST'])
+def set_completed_todo(todo_id):
+    try:
+        completed = request.get_json()['completed']
+        todo = TODO.query.get(todo_id)
+        todo.completed = completed
+        db.session.commit()
+    except:
+        db.session.rollback()
+    finally:
+        db.session.close()
+    return redirect(url_for('index'))
+
 
 
 @app.route('/')
 def index():
-    return render_template('index.html', data=TODO.query.all())
+    return render_template('index.html', data=TODO.query.order_by('id').all())
 
